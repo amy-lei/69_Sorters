1b227ea54efed10b57e97c9957d58fb820725e36
amy-lei <amy.lei2001@yahoo.com>
2019-05-06 14:03:43 -0400

    classwork

---
+++ InsertionSorter.java
@@ -0,0 +1,22 @@
+/**
+  Implement an insertion sort, in the Sorters structure
+ */
+import java.util.ArrayList;
+
+public class InsertionSorter extends Sorter {
+
+    /**
+      Construct an instance to process the user's data
+     */
+    public InsertionSorter(  ArrayList< String> usersData) {
+	super(usersData);
+    }
+    
+
+    /**
+      sort the user's data, implementing insertion sort
+     */
+    public void mySort() {
+        // your code here
+    }
+}
---
+++ SelectionSorter.java
@@ -0,0 +1,22 @@
+/**
+  Implement an insertion sort, in the Sorters structure
+ */
+import java.util.ArrayList;
+
+public class SelectionSorter extends Sorter{
+
+    /**
+      Construct an instance to process the user's data
+     */
+    public SelectionSorter(  ArrayList< String> usersData) {
+	super(usersData);
+    }
+    
+
+    /**
+      sort the user's data, implementing selection sort
+     */
+    public void mySort() {
+        // your code here
+    }
+}
